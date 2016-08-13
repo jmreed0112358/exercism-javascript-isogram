a448bac62eb12062502c16b5b57a3a83ed8d281c
Jeremy M. Reed <reeje76@gmail.com>
2016-08-12 21:48:01 -0600

    Activate all tests.  Tweak sanitize() tests.

--- isogram.spec.js
+++ isogram.spec.js
@@ -4,62 +4,62 @@ var Isogram = require('./isogram'),
 
 const UNPRINTABLE_CHARS = '\u0000\u0001\u0002\u0003\u0004\u0005\u0006\u0007\b\t\n\u000b\f\r\u000e\u000f\u0010\u0011\u0012\u0013\u0014\u0015\u0016\u0017\u0018\u0019\u001a\u001b\u001c\u001d\u001e\u001f';
 
-xdescribe('Isogram Test Suite', function () {
+describe('Isogram Test Suite', function () {
   it('duplicates', function () {
     var word = new Isogram('duplicates');
 
     expect(word.isIsogram()).toEqual(true);
   });
 
-  xit('eleven', function () {
+  it('eleven', function () {
     var word = new Isogram('eleven');
 
     expect(word.isIsogram()).toEqual(false);
   });
 
-  xit('subdermatoglyphic', function () {
+  it('subdermatoglyphic', function () {
     var word = new Isogram('subdermatoglyphic');
 
     expect(word.isIsogram()).toEqual(true);
   });
 
-  xit('Alphabet', function () {
+  it('Alphabet', function () {
     var word = new Isogram('Alphabet');
 
     expect(word.isIsogram()).toEqual(false);
   });
 
-  xit('thumbscrew-japingly', function () {
+  it('thumbscrew-japingly', function () {
     var word = new Isogram('thumbscrew-japingly');
 
     expect(word.isIsogram()).toEqual(true);
   });
 
-  xit('Hjelmqvist-Gryb-Zock-Pfund-Wax', function () {
+  it('Hjelmqvist-Gryb-Zock-Pfund-Wax', function () {
     var word = new Isogram('Hjelmqvist-Gryb-Zock-Pfund-Wax');
 
     expect(word.isIsogram()).toEqual(true);
   });
 
-  xit('Heizölrückstoßabdämpfung', function () {
+  it('Heizölrückstoßabdämpfung', function () {
     var word = new Isogram('Heizölrückstoßabdämpfung');
 
     expect(word.isIsogram()).toEqual(true);
   });
 
-  xit('the quick brown fox', function () {
+  it('the quick brown fox', function () {
     var word = new Isogram('the quick brown fox');
 
     expect(word.isIsogram()).toEqual(false);
   });
 
-  xit('Emily Jung Schwartzkopf', function () {
+  it('Emily Jung Schwartzkopf', function () {
     var word = new Isogram('Emily Jung Schwartzkopf');
 
     expect(word.isIsogram()).toEqual(true);
   });
 
-  xit('éléphant', function () {
+  it('éléphant', function () {
     var word = new Isogram('éléphant');
 
     expect(word.isIsogram()).toEqual(false);
@@ -75,18 +75,18 @@ describe('Isogram constructor', function() {
   });
 });
 
-xdescribe('sanitize()', function() {
+describe('sanitize()', function() {
   it('Removes control characters from input strings', function() {
     var word = new Isogram(UNPRINTABLE_CHARS + 'Foo Bar');
     var actual = word.sanitize();
-    var expected = 'Foo Bar';
+    var expected = 'foo bar';
     expect(actual).toEqual(expected);
   });
 
   it('Remove control characters from input strings containing unicode chars', function() {
     var word = new Isogram(UNPRINTABLE_CHARS + '¡Hola! ¿Qué tal? Привет! Iñtërnâtiônàlizætiøn☃');
     var actual = word.sanitize();
-    var expected = '¡Hola! ¿Qué tal? Привет! Iñtërnâtiônàlizætiøn☃';
+    var expected = '¡hola! ¿qué tal? привет! iñtërnâtiônàlizætiøn☃';
     expect(actual).toEqual(expected);
   });
 });
