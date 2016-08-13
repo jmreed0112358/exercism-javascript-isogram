6c135fac325288fc68aa0caa0cc0bf8291f57120
Jeremy M. Reed <reeje76@gmail.com>
2016-08-12 21:48:20 -0600

    Implement isIsogram().

--- isogram.js
+++ isogram.js
@@ -7,10 +7,30 @@ var Isogram = function(sentence) {
     throw new InvalidParameterException('This constructor takes a string!');
   }
   this.sentence = sentence;
+  this.sentence = this.sanitize();
 };
 
 Isogram.prototype.isIsogram = function() {
-  throw new NotImplementedException();
+  var i = 0, 
+    data = {},
+    regex;
+
+  xregexp.install('astral');
+  regex = xregexp('^\\pL+$');
+
+  for (i = 0 ; i < this.sentence.length ; i++ ) {
+    var letter = this.sentence[i];
+
+    if (regex.test(letter)) {
+      if (data[letter] !== undefined) {
+        return false;
+      } else {
+        data[letter] = true;
+      }
+    }
+  }
+
+  return true;
 };
 
 Isogram.prototype.sanitize = function() {
@@ -20,6 +40,7 @@ Isogram.prototype.sanitize = function() {
   xregexp.install('astral');
   regex = xregexp('^\\pC+$');
 
+  this.sentence = this.sentence.toLowerCase();
   for (i = 0 ; i < this.sentence.length ; i++ ) {
     if (!regex.test(this.sentence[i])) {
       result = result + this.sentence[i];
